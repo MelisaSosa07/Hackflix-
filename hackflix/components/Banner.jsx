c8a1b7721aca089b92ec861c3e240228c08a6b8d
Juan <juanignaciojobs@gmail.com>
2025-11-14 19:38:43 -0300

    feat: Initialize Base Structure and Components
    Implement the initial structure of the application and define essential components:
    
    - Define and create core application **components**.
    - Correctly initialize all components.
    - Implement the initial structure of the web page (initial HTML/layout).

---
+++ hackflix/components/Banner.jsx
@@ -0,0 +1,11 @@
+export default function Banner() {
+console.log("Banner component rendered");
+  return (
+    <header className="banner">
+      <div className="banner-content">
+        <h1>¡Tus películas favoritas!</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      </div>
+    </header>
+  );
+}
---
+++ hackflix/components/RatingFilter.jsx
@@ -0,0 +1,3 @@
+export default function RatingFilter() {
+  console.log("RatingFilter component rendered");
+}
